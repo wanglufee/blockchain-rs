89aa981c7587450606306e784b053eed633bb3ac
wanglufee <3812132676@qq.com>
2024-03-24 09:26:40 +0800

    添加工作量证明

--- src/block.rs
+++ src/block.rs
@@ -4,6 +4,8 @@ use crypto::digest::Digest;
 use crypto::sha2::Sha256;
 use std::time::SystemTime;
 
+const TARGET_HEXS: usize = 4;
+
 /// 区块结构体
 #[derive(Debug)]
 pub struct Block {
@@ -11,24 +13,10 @@ pub struct Block {
     data: String,
     prev_block_hash: String,
     hash: String,
+    nonce: i32,
 }
 
 impl Block {
-    /// 设置hash值
-    fn set_hash(&mut self) -> Result<()> {
-        // 计算时间戳
-        self.timestamp = SystemTime::now()
-                            .duration_since(SystemTime::UNIX_EPOCH)?
-                            .as_millis();
-        // 计算hash值
-        let content = (self.timestamp,self.data.clone(),self.prev_block_hash.clone());
-        let bytes = serialize(&content)?;
-        let mut hasher = Sha256::new();
-        hasher.input(&bytes);
-        self.hash = hasher.result_str();
-        Ok(())
-    }
-
     /// 获取区块hash值
     pub fn get_hash(&self) -> String {
         self.hash.clone()
@@ -36,14 +24,52 @@ impl Block {
 
     /// 新建区块
     pub fn new_block(data: String,prev_block_hash: String) -> Result<Block> {
+        // 计算时间戳
+        let timestamp = SystemTime::now()
+                            .duration_since(SystemTime::UNIX_EPOCH)?
+                            .as_millis();
         let mut block = Block{
-            timestamp: 0,
+            timestamp,
             data,
             prev_block_hash,
             hash: String::new(),
+            nonce: 0,
         };
-        let _ = block.set_hash();
+        let _ = block.run_proof_of_work()?;
         Ok(block)
+    }
+
+    /// 工作量证明
+    fn run_proof_of_work(&mut self) -> Result<()> {
+        while !self.validate()? {
+            self.nonce += 1;
+        }
+        let data = self.prepare_hash_data()?;
+        let mut hasher = Sha256::new();
+        hasher.input(&data);
+        self.hash = hasher.result_str();
+        Ok(())
+    }
+
+    /// 校验hash值是否合法
+    fn validate(&self) -> Result<bool> {
+        let data = self.prepare_hash_data()?;
+        let mut hasher = Sha256::new();
+        hasher.input(&data);
+        let hash = hasher.result_str();
+        let mut vec1: Vec<u8> = Vec::new();
+        vec1.resize(TARGET_HEXS, '0' as u8);
+        Ok(hash[0..TARGET_HEXS] == String::from_utf8(vec1)?)
+    }
+
+
+    /// 准备数据
+    fn prepare_hash_data(&self) -> Result<Vec<u8>> {
+        let content = (self.timestamp,self.data.clone(),
+                                                self.prev_block_hash.clone(),
+                                                self.nonce);
+        let bytes = serialize(&content)?;
+        Ok(bytes)
     }
 
     /// 生成创世区块
